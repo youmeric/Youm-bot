53ac7f5875f2e31a6b743b44a98f35ff2fd39a01
youmeric <36827232+youmeric@users.noreply.github.com>
2024-11-11 15:21:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-
+# Charger le token depuis le fichier .env
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
@@ -15,30 +15,6 @@ intents.message_content = True
 
 # Initialisation du bot avec un préfixe de commande
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-async def main():
-    # Charger les extensions pour les fonctionnalités (cogs)
-    try:
-        await bot.load_extension("clear_cog")  # Charger l'extension clear
-        print("Extension clear_cog chargée.")
-    except Exception as e:
-        print(f"Erreur de chargement clear_cog: {e}")
-
-    try:
-        await bot.load_extension("gif_cog")  # Charger l'extension gif_limit
-        print("Extension gif_cog chargée.")
-    except Exception as e:
-        print(f"Erreur de chargement gif_cog: {e}")
-
-    try:
-        await bot.load_extension("ban_gif_cog")  # Charger l'extension ban_gif
-        print("Extension ban_gif_cog chargée.")
-    except Exception as e:
-        print(f"Erreur de chargement ban_gif_cog: {e}")
-
-    # Lancement du bot avec le token
-    keep_alive()
-    await bot.start(token)  # Remplace "TON_TOKEN_ICI" par ton token réel
 
 @bot.event
 async def on_ready():
@@ -57,12 +33,36 @@ async def on_ready():
     except Exception as e:
         print(f"Erreur lors de la synchronisation des commandes slash : {e}")
 
+async def load_extensions():
+    """Fonction pour charger les extensions"""
+    try:
+        await bot.load_extension("clear_cog")
+        print("Extension clear_cog chargée.")
+    except Exception as e:
+        print(f"Erreur de chargement clear_cog: {e}")
+
+    try:
+        await bot.load_extension("gif_cog")
+        print("Extension gif_cog chargée.")
+    except Exception as e:
+        print(f"Erreur de chargement gif_cog: {e}")
+
+    try:
+        await bot.load_extension("ban_gif_cog")
+        print("Extension ban_gif_cog chargée.")
+    except Exception as e:
+        print(f"Erreur de chargement ban_gif_cog: {e}")
+
+    try:
+        await bot.load_extension("config_gif_cog")
+        print("Extension config_gif_cog chargée.")
+    except Exception as e:
+        print(f"Erreur de chargement config_gif_cog: {e}")
+
+async def main():
+    await load_extensions()  # Charger toutes les extensions
+    #keep_alive()  # Démarrer le service keep_alive si nécessaire
+    await bot.start(token)  # Démarrer le bot avec le token
+
 # Démarrage du bot avec la fonction main
 asyncio.run(main())
-
-
-
-
-
-
-#NjE5NjE4MDkxNzgwODAwNTEz.GeVzua.KWObuq4dz6uj0dIc2zMjSpeJmUgXnVna7x1fzs
